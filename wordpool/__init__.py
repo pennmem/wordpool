72579b1927cdcede4f36737f78e7b808f8d4ae5c
Michael V. DePalatis <mike@depalatis.net>
2017-03-30 14:54:24 -0400

    Begin refactor to just use DataFrames

--- wordpool/__init__.py
+++ wordpool/__init__.py
@@ -1,137 +1,100 @@
-import time
 import random
-import codecs
-import six
-
-try:
-    import pandas as pd
-except ImportError:
-    pd = None
+import numpy as np
+import pandas as pd
 
 __version__ = "0.2.dev"
 
 
-class WordList(list):
-    """A single list of words.
-
-    :param iterable: If a string type, try to open a file. Otherwise, just call
-        the normal list constructor.
-    :param dict metadata: A dict of metadata to associate with the list. If no
-        ``created`` key is given, one will be added which provides a timestamp
-        in seconds since the epoch.
-
-    """
-    def __init__(self, iterable=None, metadata=None):
-        if isinstance(iterable, (six.string_types, six.text_type)):
-            with codecs.open(iterable, encoding="utf-8") as f:
-                super(WordList, self).__init__(sorted(f.read().split()))
-        else:
-            super(WordList, self).__init__(iterable)
-
-        self.metadata = metadata or {}
-        if "created" not in self.metadata:
-            self.metadata["created"] = time.time()
-
-    def shuffle(self):
-        """Shuffle the list in place. Also returns itself to facilitate
-        chaining.
-
-        """
-        random.shuffle(self)
-        return self
-
-    def choose(self, n):
-        """Randomly select n words from the list.
-
-        :param int n: Number of words to select.
-        :returns: new :class:`WordList` instance.
-
-        """
-        assert 0 < n <= len(self)
-        words = random.sample(self, n)
-        return WordList(words)
-
-    def to_dict(self):
-        """Convert to a dict of the form::
-
-            {
-                "metadata": self.metadata,
-                "words": [<list of words in self>]
-            }
-
-        """
-        return {
-            "metadata": self.metadata,
-            "words": [word for word in self]
-        }
-
-    def to_dataframe(self):
-        """Convert to a :class:`DataFrame`. Metadata will be repeated for each
-        word in the list.
-
-        """
-        if pd is None:
-            raise RuntimeError("You must install pandas to convert to a DataFrame!")
-        words = [word for word in self]
-        d = {
-            key: [value]*len(words) for key, value in self.metadata.items()
-        }
-        d.update({"word": words})
-        return pd.DataFrame(d)
-
-    def to_text(self, filename, delimiter="\n"):
-        """Save the list of words only to a plaintext file.
-
-        :param str filename:
-        :param str delimiter:
-
-        """
-        with open(filename, "w") as outfile:
-            outfile.write(delimiter.join(self))
-
-
 class WordPool(object):
-    """Handles operations for entire word pools. Here, a word pool is
-    considered to be made up of a series one or more word lists.
+    """Utilities for working with word pools.
 
-    .. note:: Word lists must contain the same number of words!
-
-    :param list lists: Word lists to include (coerced to :class:`WordList` if
-       not already).
-    :raises: AssertionError when lists are not all the same length.
+    :param str filename: Filename containing the word pool. The word pool must
+        be in a format readable with `pd.read_table` and contain a header row
+        to name the columns; a ``word`` column is required to label the words
+        in the pool.
+    :param int n_lists: Number of lists to be made from the word pool.
 
     """
-    def __init__(self, lists):
-        self.lists = [WordList(list_) if not isinstance(list_, WordList) else list_
-                      for list_ in lists]
-        length = len(self.lists[0])
-        for list_ in self.lists:
-            assert length == len(list_)
+    def __init__(self, filename, n_lists=None):
+        self.df = pd.read_table(filename)
+        assert "word" in self.df.columns
+
+        if n_lists is not None:
+            self.assign_list_numbers(n_lists)
 
     def __str__(self):
-        return str(self.lists)
+        return str(self.df)
 
     def __len__(self):
-        return len(self.lists)
+        return len(self.df)
 
     def __iter__(self):
-        for word_list in self.lists:
-            yield word_list
+        for idx, row in self.df.iterrows():
+            yield row
 
-    def __getitem__(self, item):
-        return self.lists[item]
+    def prepend(self, words):
+        """Insert words at the beginning of the word pool.
 
-    def shuffle_lists(self, frozen=[]):
-        """Shuffle within lists in the pool (i.e., shuffle each list but do not
-        move any words between lists. Returns self.
-
-        :param list frozen: Indices of lists to not shuffle.
+        :param iterable words:
+        :returns: self
 
         """
-        for n, list_ in enumerate(self.lists):
-            if n in frozen:
-                continue
-            list_.shuffle()
+        self.df = pd.concat([words, self.df])
+        return self
+
+    def append(self, words):
+        """Append words to the end of the word pool.
+
+        :param iterable words:
+        :returns: self
+
+        """
+        self.df = pd.concat([self.df, words])
+        return self
+
+    def assign_list_numbers(self, n_lists):
+        """Assign or reassign list numbers to all words in the pool.
+
+        :param int n_lists: Total number of lists.
+
+        """
+        assert len(self.df) % n_lists == 0
+        words_per_list = int(len(self.df) / n_lists)
+        listnos = np.array(
+            [[n]*words_per_list for n in range(n_lists)]).flatten()
+        self.df["listno"] = listnos
+
+    def shuffle_words(self, frozen=[]):
+        """Shuffle words in place.
+
+        :param list frozen: List of word numbers to exclude from shuffling.
+        :returns: self
+
+        """
+        frozen_words = self.df[self.df.index.isin(frozen)]
+        shuffle_words = self.df[~self.df.index.isin(frozen)]
+        shuffled = shuffle_words.reindex(np.random.permutation(shuffle_words.index))
+        self.df = pd.concat([frozen_words, shuffled]).reset_index(drop=True)
+        return self
+
+    def shuffle_within_lists(self, frozen=[]):
+        """Shuffle within lists in the pool (i.e., shuffle each list but do not
+        move any words between lists. This requires that list
+        numbers have alreay been assigned.
+
+        :param list frozen: List numbers to not shuffle.
+
+        """
+        frozen_lists = self.df[self.df.listno.isin(frozen)]
+        shuffle_lists = self.df[~self.df.listno.isin(frozen)]
+
+        shuffled_lists = []
+        for listno in shuffle_lists.listno.unique():
+            list_ = shuffle_lists[shuffle_lists.listno == listno]
+            list_.apply(lambda col: sorted(col, key=lambda _: random.random()))
+            shuffled_lists.append(list_)
+
+        self.df = pd.concat([frozen_lists, shuffle_lists])
         return self
 
     def to_dict(self):
@@ -148,21 +111,12 @@ class WordPool(object):
             "lists": [words.to_dict() for words in self.lists]
         }
 
-    def to_dataframe(self, add_listno=True):
-        """Convert the pool to a :class:`pd.DataFrame`.
 
-        :param bool add_listno: When True, include a `listno` column to keep
-            track of which list number a given word is from.
-
-        """
-        if pd is None:
-            raise RutimeError("Please install Pandas")
-
-        dfs = []
-        for listno, list_ in enumerate(self.lists):
-            df = list_.to_dataframe()
-            if add_listno:
-                df["listno"] = [listno]*len(list_)
-            dfs.append(df)
-
-        return pd.concat(dfs)
+if __name__ == "__main__":
+    import os.path as osp
+    pool = WordPool(osp.join(osp.dirname(__file__), "data", "ram_wordpool_en.txt"))
+    pool.shuffle_words()
+    print(pool.head())
+    pool.assign_list_numbers(25)
+    pool.shuffle_within_lists()
+    print(pool.head())
